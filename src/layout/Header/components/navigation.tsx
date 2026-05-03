a51970f91914459b27bb7074b84a1d8c7992db20
Sharti <mixa102006@ya.ru>
2026-05-03 19:27:15 +0300

    header footer and page contacts adaptived

--- src/layout/Header/components/navigation.tsx
+++ src/layout/Header/components/navigation.tsx
@@ -1,7 +1,11 @@
 import { NavLink } from "react-router-dom";
 import { NavigationList } from "../../../lists/Navigation";
 import Logo from "../../../static/icons/black-logo.png";
+import PhoneIcon from "../../../static/icons/white-phone-icon.png";
+import MenuArrow from "../../../static/icons/arrow-right.png";
+import { useState } from "react";
 export default function Navigation() {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   const copyToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
@@ -15,7 +19,10 @@ export default function Navigation() {
       <section id="logo-wrapper">
         <img src={Logo} alt="Креатив+ - рекламное агенство" id="logo-image" />
       </section>
-      <section id="header-navigation-menu">
+      <section
+        id="header-navigation-menu"
+        className={`${isVisible ? "nav-menu-visible" : ""}`}
+      >
         {NavigationList.map((item) => (
           <NavLink
             key={item.id}
@@ -23,16 +30,32 @@ export default function Navigation() {
             className={({ isActive }) =>
               `header-navigation-element ${isActive ? "active" : ""}`
             }
+            onClick={() => setIsVisible(false)}
           >
             {item.title}
           </NavLink>
         ))}
       </section>
-      <section
-        id="header-phone-wrapper"
-        onClick={() => copyToClipboard("+74842751906")}
-      >
-        <p id="header-phone-link">+7 (4842) 75-19-06</p>
+      <section id="header-phone-wrapper">
+        <p
+          id="header-phone-link"
+          onClick={() => copyToClipboard("+74842751906")}
+        >
+          +7 (4842) 75-19-06
+        </p>
+        <div
+          id="header-phone-icon-wrapper"
+          onClick={() => copyToClipboard("+74842751906")}
+        >
+          <img src={PhoneIcon} alt="Иконка телефона" id="phone-icon-image" />
+        </div>
+        <div
+          id="header-navigation-button"
+          onClick={() => setIsVisible(!isVisible)}
+          className={`${isVisible ? "nav-rotaded" : ""}`}
+        >
+          <img src={MenuArrow} alt="Кнопка менюшки" />
+        </div>
       </section>
     </aside>
   );
